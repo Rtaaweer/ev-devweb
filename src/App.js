96d5e646f0c665f556b4ff670564c627ff8fbb93
Rtaaweer <richard.torresj7@gmail.com>
2025-06-18 14:34:57 -0600

    Changes in the design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="landing-title">Welcome - Landing Page</h1>
+        <p className="landing-subtitle">Your journey starts here</p>
         <a
           className="App-link"
           href="https://reactjs.org"
