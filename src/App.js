46867ba5499de52bc839302ced8e1ce39b65c24b
Rtaaweer <richard.torresj7@gmail.com>
2025-06-18 14:47:09 -0600

    More changes in the design

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="landing-title">Welcome - Landing Page</h1>
-        <p className="landing-subtitle">Your journey starts here</p>
+        <p className="landing-subtitle">This a simple or static page for the evaluation of unit 2</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Rtaaweer/ev-devweb/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Link 
         </a>
       </header>
     </div>
